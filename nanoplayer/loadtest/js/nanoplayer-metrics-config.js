c1d1c8cab58d78fbc3048e0f3e5649b4bf90e2f0
Thomas Niedergesaess <tn@nanocosmos.de>
2019-03-14 16:33:55 +0100

    chore(loadtest): add loadtest player

---
+++ nanoplayer/loadtest/js/nanoplayer-metrics-config.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    var useMetrics = true;
+    if (useMetrics) {
+        window.nanoPlayerMetricsConfig = {
+            accountId: 'nanocosmosloadtest',
+            accountKey: 'nclt2gxl52fg0r4s',
+            userId: 'loadtest',
+            eventId: 'loadtest',
+            statsInterval: 10,
+            customField1: 'loadtest',
+            customField2: 'metrics',
+            customField3: 'internal'
+        }
+    }
+})(window);
