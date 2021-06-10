6b4cedf61fb2f9fa99dc674855a9f6707a08d1ac
Thomas Niedergesaess <tniedergesaess@nanocosmos.de>
2021-06-10 21:12:21 +0200

    chore(version): embed player 1.0.0

---
+++ nanoplayer/embed/1.0.0/js/nanoplayer/nanoplayer-main.js
@@ -0,0 +1,20 @@
+// nanoplayer-main
+// nanoplayer main entry
+// (c) 2021, nanocosmos gmbh
+// https://www.nanocosmos.de
+
+// NanoPlayer Embed 1.0.0
+
+/* eslint-disable no-undef, no-console, no-unused-vars */
+// This file is necessary to setup player with config properly. Please don't change anything here.
+var nanoPlayer;
+var nanoPlayerMain = function () {
+    console.log('NanoPlayer Embed Version ' + nanoPlayerEmbedVersion);
+    nanoPlayer = new NanoPlayer('playerDiv');
+    // nanoPlayerConfig from NANOPLAYER-CONFIG.JS
+    nanoPlayer.setup(nanoPlayerConfig).then(function (config) {
+        console.log('Setup: ' + JSON.stringify(config, undefined, 4));
+    }, function (error) {
+        console.error(error.message);
+    });
+};
