fbd91a7492a2b6649030347fde5415531e4fdfb4
Thomas Niedergesaess <tniedergesaess@nanocosmos.de>
2023-06-26 09:18:06 +0200

    chore(build): correct version string

--- nanoplayer/embed/1.3.2/js/nanoplayer/nanoplayer-embed-version.js
+++ nanoplayer/embed/1.3.2/js/nanoplayer/nanoplayer-embed-version.js
@@ -2,7 +2,7 @@
 // (c) 2021 - 2022, nanocosmos gmbh
 // https://www.nanocosmos.de
 
-// NanoPlayer Embed 1.3.1
+// NanoPlayer Embed 1.3.2
 
 /* eslint-disable no-undef, no-console, no-unused-vars */
-var nanoPlayerEmbedVersion = '1.3.1';
+var nanoPlayerEmbedVersion = '1.3.2';
