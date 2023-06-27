58d00e306de359bb77a80f4786eefa5bcace1c72
Thomas Niedergesaess <tniedergesaess@nanocosmos.de>
2023-06-27 13:43:45 +0200

    chore(version): 1.3.3

---
+++ nanoplayer/embed/1.3.3/js/nanoplayer/nanoplayer-embed-version.js
@@ -0,0 +1,8 @@
+// nanoplayer-embed-version
+// (c) 2021 - 2023, nanocosmos gmbh
+// https://www.nanocosmos.de
+
+// NanoPlayer Embed 1.3.3
+
+/* eslint-disable no-undef, no-console, no-unused-vars */
+var nanoPlayerEmbedVersion = '1.3.3';
