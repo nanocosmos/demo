998e6a2e4e486a05f17cabe5d9d6384c44560612
tn <tn@nanocosmos.de>
2025-04-03 15:02:06 +0200

    chore(version): 1.3.4

---
+++ nanoplayer/embed/1.3.4/js/nanoplayer/nanoplayer-embed-version.js
@@ -0,0 +1,8 @@
+// nanoplayer-embed-version
+// (c) 2021 - 2025, nanocosmos gmbh
+// https://www.nanocosmos.de
+
+// NanoPlayer Embed 1.3.4
+
+/* eslint-disable no-undef, no-console, no-unused-vars */
+var nanoPlayerEmbedVersion = '1.3.4';
